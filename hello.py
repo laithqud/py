e2ca75eaa90283caf6481db6eef17393f2e675eb
AhmadQud13 <ahmad.qud90@gmail.com>
2026-05-25 20:48:56 +0300

    update

--- hello.py
+++ hello.py
@@ -1,9 +1,2 @@
 greeting = 'Hello World!'
-# print(greeting)
-# print("welcome back to python course!!")
-# print("updating the code")
-print("git init")
-print("git add .")
-print("git commit -m 'first commit'")
-print ("git remote add origin <remote_url>")
-print("git push origin main")
+print(greeting)
